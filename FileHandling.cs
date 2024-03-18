3f3e8c1a7743f9e8347295b9b29023cff71bc667
Tom <82216583+Tr0-p@users.noreply.github.com>
2024-03-18 21:04:11 +0000

    Inserted functionality to read the files.

---
+++ FileHandling.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AlgorithmsAssessment
+{
+    public class FileHandling
+    {
+        private string _basePath =
+            @"C:\Users\tommi\RiderProjects\AlgorithmsAssessment\AlgorithmsAssessment\NetworkFiles";
+        
+        public List<int> ReadFileIntoArray(string fileName)
+        {
+            string path = Path.Combine(_basePath, fileName);
+            
+            if (File.Exists(path))
+            {
+                string[] fileLines = File.ReadAllLines(path);
+                List<int> allNumbers = new List<int>();
+                
+                foreach (string number in fileLines)
+                {
+                    allNumbers.Add(int.Parse(number));
+                }
+
+                return allNumbers;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"File {path} cannot be found! Maybe check the directory?");
+                Console.ResetColor();
+
+                return new List<int>();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace AlgorithmsAssessment
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Versioning;
+using AlgorithmsAssessment;
+
+namespace AlgorithmsAssessment
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            FileHandling fileHandler = new FileHandling();
+
+            List<int> net1 = fileHandler.ReadFileIntoArray("Net_1_256.txt");
+            List<int> net2 = fileHandler.ReadFileIntoArray("Net_2_256.txt");
+            List<int> net3 = fileHandler.ReadFileIntoArray("Net_3_256.txt");
             
+            
+
         }
     }
 }
