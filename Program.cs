df30371baf15d2f88646a9c270e8bb429fe04e7a
Tom <82216583+Tr0-p@users.noreply.github.com>
2024-03-20 20:34:23 +0000

    Final (Excluding Steps)

--- Program.cs
+++ Program.cs
@@ -8,13 +8,19 @@ namespace AlgorithmsAssessment
 {
     internal class Program
     {
-        private static int MenuHandleAndVerify(string[] menu, IOHandler io)
+        private static int MenuHandleAndVerify(string[] menu, IOHandler io, int excludedOption = -1)
         {
             (bool, int) choice = (false, 0);
 
             while (!choice.Item1)
             {
                 choice = io.HandleMenu(menu);
+                
+                if (choice.Item2 == excludedOption)
+                {
+                    io.WriteColourTextLine("This option cannot be chosen at the moment.", ConsoleColor.DarkRed);
+                    choice = (false, choice.Item2);
+                }
             }
 
             return choice.Item2;
@@ -22,19 +28,52 @@ namespace AlgorithmsAssessment
 
         private static void AscendingAndDescendingOutput(List<int> array, IOHandler io)
         {
+            int jump = 0;
+
+            switch (array.Count)
+            {
+                case 2048:
+                    jump = 50;
+                    break;
+                
+                case 4096:
+                    jump = 50;
+                    break;
+                
+                default:
+                    jump = 10;
+                    break;
+                
+            } 
+            
             io.WriteColourTextLine("\n\nAscending Order", ConsoleColor.DarkGreen);
-            for (int i = 0; i < array.Count; i += 10)
+            for (int i = 0; i < array.Count; i += jump)
             {
                 io.WriteColourTextLine($"Value {i}: {array[i]}", ConsoleColor.Green);
             }
             
             Console.WriteLine();
             
-            io.WriteColourTextLine("Ascending Order", ConsoleColor.DarkRed);
-            for (int i = array.Count - 1; i >= 0; i -= 10)
+            io.WriteColourTextLine("Descending Order", ConsoleColor.DarkRed);
+            for (int i = array.Count - 1; i >= 0; i -= jump)
             {
                 io.WriteColourTextLine($"Value {i}: {array[i]}", ConsoleColor.Red);
             }
+        }
+
+        private static List<int> MergeLists(List<List<int>> arrays)
+        {
+            List<int> mergeArray = new List<int>();
+
+            foreach (List<int> list in arrays)
+            {
+                foreach (int number in list)
+                {
+                    mergeArray.Add(number);
+                }
+            }
+
+            return mergeArray;
         }
         
         public static void Main(string[] args)
@@ -44,15 +83,41 @@ namespace AlgorithmsAssessment
             Sorting sort = new Sorting();
             Searching search = new Searching();
             
-            string[] arrayOptions = {"Net_1_256", "Net_2_256", "Net_3_256"};
+            string[] arrayOptions = {"Net_1_256.txt", "Net_2_256.txt", "Net_3_256.txt", "Net_1_2048.txt", "Net_2_2048.txt", "Net_3_2048.txt", "Merge Two Arrays"};
             String[] sortMenu = { "Bubble Sort", "Insertion Sort", "Merge Sort", "Quick Sort" };
             String[] searchMenu = { "Linear Search", "Binary Search" };
-            List<int>[] arrays = { fileHandler.ReadFileIntoArray("Net_1_256.txt"), fileHandler.ReadFileIntoArray("Net_2_256.txt"), fileHandler.ReadFileIntoArray("Net_3_256.txt") };
+            List<List<int>> arrays = new List<List<int>>();
+
+            foreach (string fileName in arrayOptions)
+            {
+                if (fileName.Substring(fileName.Length - 3) == "txt")
+                {
+                    arrays.Add(fileHandler.ReadFileIntoArray(fileName));
+                }
+            }
             
             while (true)
             {
                 int arrayChosen = MenuHandleAndVerify(arrayOptions, io);
-                List<int> arrayToHandle = arrays[arrayChosen];
+                List<int> arrayToHandle = new List<int>();
+                
+                if (arrayChosen == 6)
+                {
+                    io.WriteColourTextLine("\nChoose the first array to merge.", ConsoleColor.DarkCyan);
+                    int arrayOptionOne = MenuHandleAndVerify(arrayOptions, io, 6);
+                    
+                    io.WriteColourTextLine("\nChoose the second array to merge.", ConsoleColor.DarkCyan);
+                    int arrayOptionsTwo = MenuHandleAndVerify(arrayOptions, io, 6);
+
+                    arrayToHandle = MergeLists(new List<List<int>> { arrays[arrayOptionOne], arrays[arrayOptionsTwo] });
+                    io.WriteColourTextLine("Successfully merged the two arrays!", ConsoleColor.Green);
+                }
+
+                if (arrayToHandle.Count == 0)
+                {
+                    arrayToHandle = arrays[arrayChosen];
+                }
+                
                 List<int> sortedList = arrayToHandle;
                 
                 int sortOption = MenuHandleAndVerify(sortMenu, io);
